a75fdc2fb724d60a96179323c7843020441a453f
Igors Krasjukovs <tahq69@gmail.com>
2017-11-29 22:32:34 +0200

    tags component added - non working;

---
+++ src/Tags.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import Vue from "vue"
+import Component from "vue-class-component"
+import { Prop, Watch } from "vue-property-decorator"
+
+import CripOption from "./Option"
+
+@Component({ name: "CripTags" })
+export default class CripTags<T> extends Vue {
+  public tags: CripOption<T>[] = []
+}
+</script>
+
+<template>
+  <div class="input-group-btn">
+    <button
+        v-for="tag in tags"
+        :key="tag.key"
+        class="btn btn-default"
+    >
+      {{ tag.plainText() }}
+    </button>
+  </div>
+</template>
+
